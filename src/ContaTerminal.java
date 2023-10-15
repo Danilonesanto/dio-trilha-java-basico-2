05d0449799ee188a9c586b61f83184b1be717158
Danilonesanto <danilo.espirito@gmail.com>
2023-10-14 21:27:00 -0300

    Commit contaTerminal

---
+++ src/ContaTerminal.java
@@ -0,0 +1,27 @@
+Danilo import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+        
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+            
+            System.out.println("Por favor, digite o seu nome");
+            String nomeCliente = scanner.nextLine();
+
+            System.out.println("Por favor, digite o numero da agencia");
+            String agencia = scanner.next();
+
+            System.out.println("Por favor, digite o numero da conta");
+            int numero = scanner.nextInt();
+
+            System.out.println("Por favor, digite o numero do saldo");
+            Double saldo = scanner.nextDouble();
+
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível");
+
+        
+    }
+}
